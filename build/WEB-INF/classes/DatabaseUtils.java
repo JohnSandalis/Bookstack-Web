15c2bf98bbc5edb6d9c9c5e33af04593cafbc74d
GiorgLag <t8190087@aueb.gr>
2021-12-11 14:58:03 +0200

    Add methods to retrieve book subjects from database

--- build/WEB-INF/classes/DatabaseUtils.java
+++ build/WEB-INF/classes/DatabaseUtils.java
@@ -21,6 +21,8 @@ public class DatabaseUtils {
     private static final String SELECT_SUBMITTED_BOOKS = "SELECT * FROM books_submitted";
     private static final String SELECT_BOOK_WITH_ISBN = "SELECT * FROM books WHERE isbn = '%s'";
     private static final String SELECT_AUTHORS_OF_BOOK = "SELECT * FROM authors WHERE isbn = '%s'";
+    private static final String SELECT_BOOK_SUBJECTS = "SELECT * FROM books_subjects WHERE isbn = '%s'";
+    private static final String SELECT_SUBJECT_FROM_ID = "SELECT * FROM subjects WHERE id = '%d%";
 
     // Returns an active connection object to the project's database
     private static Connection createDatabaseConnection() throws SQLException {
@@ -74,16 +76,17 @@ public class DatabaseUtils {
                 Date publish_date = rs.getDate("publish_date");
                 String lang = rs.getString("lang");
                 List<String> authors = getAuthorsOfBook(isbn);
+                List<String> subjects = getBookSubjects(isbn);
                 return new Book(isbn, title, subtitle, pages_count, thumbnail_url, publisher,
-                        publish_date, lang, authors);
+                        publish_date, lang, authors, subjects);
             }
             return null;
         }
     }
 
     /**
-     *
-     * @param isbn The unique ISBN of the book's authors that we're looking for
+     * Creates a connection to the database and gets the authors of the book with the corresponding isbn
+     * @param isbn The unique ISBN of the book that we want the authors of
      * @return the names of the authors
      * @throws SQLException when a connection to the database cannot be established
      */
@@ -97,6 +100,41 @@ public class DatabaseUtils {
                 authorNames.add(name);
             }
             return authorNames;
+        }
+    }
+
+    /**
+     * Creates a connection to the database and gets the subjects of the book with the corresponding isbn
+     * @param isbn The unique ISBN of the book that we want the subjects of
+     * @return the subjects of the book
+     * @throws SQLException when a connection to the database cannot be established
+     */
+    public static List<String> getBookSubjects(String isbn) throws SQLException {
+        try(Connection connection = createDatabaseConnection()) {
+            List<String> bookSubjects = new ArrayList<>();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(String.format(SELECT_BOOK_SUBJECTS, isbn));
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String subject = getSubjectFromId(id);
+                bookSubjects.add(subject);
+            }
+            return bookSubjects;
+        }
+    }
+
+    /**
+     * Creates a connection to the database and gets the subject with the corresponding id
+     * @param id the id of the subjects table
+     * @return the subject of the corresponding id
+     */
+    public static String getSubjectFromId(int id) throws  SQLException {
+        try(Connection connection = createDatabaseConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(String.format(SELECT_SUBJECT_FROM_ID, id));
+            if (rs.next()) {
+                return rs.getString("name");
+            }
         }
     }
 }
