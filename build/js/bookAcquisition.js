305be1abc192c016c860be11cc1cd331442655da
sotirangelo <sotirangelo@github.com>
2022-01-27 19:04:17 +0200

    Fix typo in jQuery

--- build/js/bookAcquisition.js
+++ build/js/bookAcquisition.js
@@ -1,5 +1,5 @@
 jQuery( () => {
-    $("confirmation-btn").on("click", function() {
+    $("#confirmation-btn").on("click", function() {
         if(confirm("You are about to spend credits to get this book."
         + "Are you sure you want to proceed?")) {
             console.log("OK");
