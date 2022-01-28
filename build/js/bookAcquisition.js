9b112b79e32698b92e8cfcaafeef5488acd6a02b
Zoi Aposkiti <zoi.aposkiti@gmail.com>
2022-01-28 20:18:42 +0200

    Attempt new fix for acquisition

--- build/js/bookAcquisition.js
+++ build/js/bookAcquisition.js
@@ -1,10 +1,9 @@
-jQuery( () => {
-    $("#acquire_form").submit(function(event) {
-	if confirm("You are about to spend credits to get this book."         		+ " Are you sure you want to proceed?") {
+function confirmAcquisition() {
+        if confirm("You are about to spend credits to get this book."
+		+ " Are you sure you want to proceed?") {
 
-		alert("Successful book acquisition!");
-
-	else {
-		event.preventDefault();
+                alert("Successful book acquisition!");
+		return true;
+        else {
+        	return false;
 	}
-})
